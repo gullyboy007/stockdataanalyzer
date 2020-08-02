fc345e55a9b312b16af3b505424e0a3f4bbaf904
gullyboy007 <abhineet.anand.2000@gmail.com>
2020-08-02 23:01:12 +0530

    minor fixes

--- data-downloader.py
+++ data-downloader.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import yfinance as yf
+from datetime import date , timedelta
 
 
 df = pd.read_csv('NSE-EQ-SYMBOLS.csv')
@@ -9,19 +10,31 @@ sym = []
 sym = df['SYMBOL'].tolist()
 
 
-def data_downloader(symbol):
-    path = '/home/abhineet/py_files/PyPortfolioBuilder/nse_data'
 
-    for i in range(0,len(symbol)):
-        try:
-            tickerData = yf.download(symbol[i]+'.NS'start="2020-7-20", end="2020-7-24", interval='1m')
-            file_name = symbol[i] + '.csv'
-            if tickerData['Open'][-1] > 30:
-                tickerData.to_csv(os.path.join(path,file_name))
-            else:
-                print("error in else")
+def data_downloader(symbol):
+    path = '/home/abhineet/py_files/PyPortfolioBuilder/stockdataanalyzer/nse-onemin-data'
+    startTime = date(2020, 7, 3)
+    nextTime = startTime + timedelta(days=+6)
+    print(startTime)
+    print(nextTime)
+
+    while(startTime <= date.today()):
+
+        for i in range(0,len(symbol)):
+            try:
+                tickerData = yf.download(symbol[i]+'.NS',start='2020-7-3', end='2020-7-6', interval='1m')
+                file_name = symbol[i] + '.txt'
+                filePath = os.path.join(path,file_name)
+                if os.path.exists(filePath):
+                    append_write = 'a' # append if already exists
+                else:
+                    append_write = 'w' # make a new file if not
+                with open(filePath,append_write) as outfile:
+                    df.to_string(outfile)
+                outfile.close()
+            except:
+                print("error in except")
                 continue
-        except:
-            print("error in except")
-            continue
+        startTime = startTime + timedelta(days=+6)
+        
 data_downloader(symbol=sym)
