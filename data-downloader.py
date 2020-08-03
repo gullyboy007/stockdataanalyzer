e638aa82212fcb168f03c6156b30fc666e532cbf
gullyboy007 <abhineet.anand.2000@gmail.com>
2020-08-03 18:39:27 +0530

    major fixes to time input

--- data-downloader.py
+++ data-downloader.py
@@ -13,16 +13,27 @@ sym = df['SYMBOL'].tolist()
 
 def data_downloader(symbol):
     path = '/home/abhineet/py_files/PyPortfolioBuilder/stockdataanalyzer/nse-onemin-data'
-    startTime = date(2020, 7, 3)
+    startTime = date(2020, 7, 6)
     nextTime = startTime + timedelta(days=+6)
-    print(startTime)
-    print(nextTime)
+    
 
     while(startTime <= date.today()):
 
+        strTime = str(startTime)
+        strNextTime = str(nextTime)
+        if strTime[5] == '0' :
+            strTime = strTime[:5] + strTime[6:]
+        if strTime[8] == '0' :
+            strTime = strTime[:8] + strTime[9:]
+        if strNextTime[5] == '0' :
+            strNextTime = strNextTime[:5] + strNextTime[6:]
+        if strNextTime[8] == '0' :
+            strNextTime = strNextTime[:8] + strNextTime[9:]            
+
+
         for i in range(0,len(symbol)):
             try:
-                tickerData = yf.download(symbol[i]+'.NS',start='2020-7-3', end='2020-7-6', interval='1m')
+                tickerData = yf.download(symbol[i]+'.NS',start=strTime, end=strNextTime, interval='1m')
                 file_name = symbol[i] + '.txt'
                 filePath = os.path.join(path,file_name)
                 if os.path.exists(filePath):
@@ -32,13 +43,15 @@ def data_downloader(symbol):
                 try:
 
                     with open(filePath,append_write) as outfile:
-                        tickerData.to_string(outfile)
+                        tickerData.to_string(outfile, header=False)
                     outfile.close()
                 except:
+                    print("Error in Txt")
                     continue    
             except:
                 print("Error in Symbol")
                 continue
         startTime = startTime + timedelta(days=+6)
+    print("End of Program")    
         
 data_downloader(symbol=sym)
